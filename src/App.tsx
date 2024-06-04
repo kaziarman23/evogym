ac2e856ff1f2aed2fc23ead0bfdc6a19267d48b3
kaziarman23 <kaziarman@proton.me>
2024-06-05 01:00:36 +0600

    navbar part is complite

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-
+import { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { SelectedPage } from "./Shared/Types";
 
 function App() {
-  
+    const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+        SelectedPage.Home
+    );
+    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  return (
-    <>
-     
-    </>
-  )
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage(SelectedPage.Home);
+            } else if (window.scrollY !== 0) setIsTopOfPage(false);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);   
+    return (
+        <>
+            <div className="app bg-gray-20 ">
+                <Navbar
+                    isTopOfPage={isTopOfPage}
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
+
+// packages : npm i framer-motion react-anchor-link-smooth-scroll@1.0.12 @heroicons/react
+//  framer-motion = for animetion
+//  react-anchor-link-smooth-scroll@1.0.12 = for clicking in the nevbar and take it to other places
+//  @heroicons/react = for icons
+
+//dependencies : npm i -D @types/react-anchor-link-smooth-scroll@1.0.2 @types/node
+
+// @types/react-anchor-link-smooth-scroll@1.0.2 = for typeScript indentify the types of code are exist in this package
+
+// @types/node = for package modules
